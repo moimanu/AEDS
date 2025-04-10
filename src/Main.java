77585ef42ca71aae1025b7cc4d07fb369556d847
Moisés Emanuel <moisesemanuelreisdacruz@hotmail.com>
2025-04-10 02:56:51 -0300

    Comentando o SelectionSort + Adicionando quebra de linha na IDE.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,40 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static void main(String[] args) {
+
+        int vetor[] = {2,0,3,4,1,6,5,7};
+        selectionSort(vetor, 0, 7);
+
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.println(vetor[i]);
         }
+
+    }
+
+    /*
+        SelectionSort:
+
+        Basicamente, o Selection Sort fixa uma primeira chave (nesse caso, o valor do menor índice), e uma busca no restante do vetor para encontrar uma outra chave que seja menor. Ao final do primeiro ciclo de busca, a menor chave encontrada será trocada com a chave fixada inicialmente. Depois disso, será preciso fixar mais uma chave (desconsiderando a primeira posição), e realizar o mesmo processo com o restante do vetor. Esse metodo pode ser criado de maneira recursiva ou iterativa.
+    */
+
+    public static void selectionSort(int[]V, int iMenor, int iMaior) {
+
+        if (iMenor >= iMaior) return; //Serve para evitar recursão infinita
+
+        int possivelMenor = iMenor; //Inicia com uma hipotese
+
+        for(int i = iMenor + 1; i <= iMaior; i++) { //Encontra uma chave menor que a hipotese
+            if(V[i] < V[possivelMenor]){
+                possivelMenor = i;
+            }
+        }
+
+        if (V[iMenor] != V[possivelMenor]) { //Esse codigo é estável, mas a versão clássica não é
+            int aux = V[iMenor]; //Guarda a chave inicial
+            V[iMenor] = V[possivelMenor]; //Coloca a menor chave na posicao inicial
+            V[possivelMenor] = aux; //Troca respectivamente
+        }
+
+        selectionSort(V, iMenor + 1, iMaior); //Comeca novamente, mas agora com o proximo indice
     }
 }
